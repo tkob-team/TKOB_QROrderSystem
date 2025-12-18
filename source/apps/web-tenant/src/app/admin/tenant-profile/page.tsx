fd070995442e7f56f1fecf65dd3ba51c5accdf21
TonKnight <tanhiep24135@gmail.com>
2025-12-18 13:52:05 +0700

    [FE][chore] Add Tenant Profile management page and component

---
+++ source/apps/web-tenant/src/app/admin/tenant-profile/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import { TenantProfilePage } from '@/features/settings/components/TenantProfilePage';
+
+export default function TenantProfile() {
+  return <TenantProfilePage />;
+}
